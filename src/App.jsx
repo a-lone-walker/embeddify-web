80fd275e045bbba346ab61f9c218d910e1a09c87
unknown <kadagalaashok414@gmail.com>
2026-03-11 12:32:23 +0530

    first version of embeddify solutions website

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Embeddify Solutions</h1>
+      <p>Embedded Systems | IoT | Robotics</p>
+    </div>
+  )
+}
+
+export default App
